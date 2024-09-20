945d97d38eebf45da81737155b083875d8698d43
naim2592 <mddurjoyalok@gmail.com>
2024-09-20 22:27:12 +0600

    3rd

--- js/home.js
+++ js/home.js
@@ -1,1 +1,71 @@
-console.log('home js aded');
+// toggle
+document.getElementById('add-btn').addEventListener('click',function(){
+    const cashIn=document.getElementById('cashin')
+    cashIn.classList.remove('hidden')
+    const cashOut=document.getElementById('cashout')
+    cashOut.classList.add('hidden')
+    
+})
+// out
+document.getElementById('out-btn').addEventListener('click',function(){
+    const cashOut=document.getElementById('cashout')
+    cashOut.classList.remove('hidden')
+    const cashIn=document.getElementById('cashin')
+    cashIn.classList.add('hidden')
+    
+})
+
+
+
+
+
+document.getElementById('addmoney-btn').addEventListener('click',function(event){
+    event.preventDefault() ;
+    const amount=document.getElementById('amount').value
+    const pin=document.getElementById('pin').value
+    if(pin==='6246'){
+      const previous=document.getElementById('pBalance').innerText
+      
+      const  pBallance=Number(previous)
+      const addBalance=Number(amount)
+      const newBalance=pBallance+addBalance
+      console.log(newBalance);
+      document.getElementById('pBalance').innerText=newBalance
+      
+        
+    }
+    else{
+          alert('Sorry money is not added, try again later')
+    }
+
+    
+    
+    
+    
+})
+// cashout js
+
+document.getElementById('cashout-btn').addEventListener('click',function(event){
+    event.preventDefault() ;
+    const amount=document.getElementById('cashout-amount').value
+    const pin=document.getElementById('cashout-pin').value
+    if(pin==='6246'){
+      const previous=document.getElementById('pBalance').innerText
+      
+      const  pBallance=Number(previous)
+      const addBalance=Number(amount)
+      const newBalance=pBallance-addBalance
+      console.log(newBalance);
+      document.getElementById('pBalance').innerText=newBalance
+      
+        
+    }
+    else{
+          alert('Sorry money is not added, try again later')
+    }
+
+    
+    
+    
+    
+})
